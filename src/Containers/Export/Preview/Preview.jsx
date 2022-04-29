d2aaa99a731700e9b25c708ffc6cc1db31915694
Jaison Rego <jaison.rego@robosoftin.com>
2022-04-29 16:38:45 +0530

    Integrated the Portrait Page UI.

--- src/Containers/Export/Preview/Preview.jsx
+++ src/Containers/Export/Preview/Preview.jsx
@@ -1,16 +1,42 @@
-import "./Preview.scss";
+import React, { PureComponent } from "react";
+import ReactCrop from "react-image-crop";
+import "react-image-crop/src/ReactCrop.scss";
 
-const Preview = () => {
-  return (
-    <>
+import "./Preview.scss";
+class Preview extends PureComponent {
+  state = {
+    src: "https://i.picsum.photos/id/212/700/700.jpg?hmac=GrEX7n4bVOZt4IjO2_WLLcoiQvPMQT7Sb8QfuXBjKoA",
+    crop: {
+      unit: "%",
+      width: 300,
+      aspect: 16 / 9,
+    },
+  };
+
+  onCropChange = (crop, percentCrop) => {
+    this.setState({ crop });
+  };
+
+  render() {
+    const { crop, src } = this.state;
+    console.log(crop);
+    return (
       <div className="preview-wrapper">
-        <img
-          class="image-preview"
-          src="https://i.picsum.photos/id/212/700/700.jpg?hmac=GrEX7n4bVOZt4IjO2_WLLcoiQvPMQT7Sb8QfuXBjKoA"
-        />
+        {/* <div style={{ position: "absolute" }}> */}
+        {/* <img
+            className={"image-preview " + this.props.setCropClass}
+            src="https://i.picsum.photos/id/212/700/700.jpg?hmac=GrEX7n4bVOZt4IjO2_WLLcoiQvPMQT7Sb8QfuXBjKoA"
+          /> */}
+        {/* <img
+            className={"image-preview"}
+            src="https://i.picsum.photos/id/212/700/700.jpg?hmac=GrEX7n4bVOZt4IjO2_WLLcoiQvPMQT7Sb8QfuXBjKoA"
+          /> */}
+        {/* </div> */}
+        {/* <div className={"update-class " + this.props.setCropClass}></div> */}
+        <ReactCrop src={src} crop={crop} onChange={this.onCropChange} />
       </div>
-    </>
-  );
-};
+    );
+  }
+}
 
 export default Preview;
