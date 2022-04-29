af098ea3b78cc3b495eab7ca6a02bd4799594adb
Jaison Rego <jaison.rego@robosoftin.com>
2022-04-29 11:12:59 +0530

    Added the static image.

---
+++ src/Containers/Export/Preview/Preview.jsx
@@ -0,0 +1,16 @@
+import "./Preview.scss";
+
+const Preview = () => {
+  return (
+    <>
+      <div className="preview-wrapper">
+        <img
+          class="image-preview"
+          src="https://i.picsum.photos/id/212/700/700.jpg?hmac=GrEX7n4bVOZt4IjO2_WLLcoiQvPMQT7Sb8QfuXBjKoA"
+        />
+      </div>
+    </>
+  );
+};
+
+export default Preview;
---
+++ src/Containers/Export/SideBar/ImageCrop/ImageCrop.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class ImageCrop extends Component {
+  state = {};
+  render() {
+    return <div>Inside Crop</div>;
+  }
+}
+
+export default ImageCrop;
