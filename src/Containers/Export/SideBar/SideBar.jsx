c1d5088ad82edb19b5f4a111117cf87d03638574
akshitha23 <akshithasalian8@gmail.com>
2022-04-29 18:49:06 +0530

    Frame Component

--- src/Containers/Export/SideBar/SideBar.jsx
+++ src/Containers/Export/SideBar/SideBar.jsx
@@ -1,13 +1,16 @@
-import "./SideBar.scss";
-import ImageCrop from "./ImageCrop/ImageCrop";
+import './SideBar.scss';
+import ImageCrop from './ImageCrop/ImageCrop';
+import Frame from './Frame/Frame.jsx';
 
-const SideBar = (props) => {
+const SideBar = props => {
   const { cropImage, selectedIcon } = props;
   return (
     <>
-      <div className="SideBar-wrapper">
-        SideBar!!
-        <ImageCrop cropImage={cropImage} selectedIcon={selectedIcon} />
+      <div className='SideBar-wrapper'>
+        {/* Either of the  Components ImageCrop or Frame should be shown at a time.
+        Comment or uncomment the required Component accordingly.*/}
+        {/* <ImageCrop cropImage={cropImage} selectedIcon={selectedIcon} /> */}
+        <Frame />
       </div>
     </>
   );
