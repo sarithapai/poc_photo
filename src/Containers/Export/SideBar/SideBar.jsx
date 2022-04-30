540247757c444811584c29567ff2c9eef94ccfc4
Saritha Shenoy <saritha.shenoy@robosoftin.com>
2022-04-30 17:07:34 +0530

    Fixed: In Image crop UI image was not visible.

--- src/Containers/Export/SideBar/SideBar.jsx
+++ src/Containers/Export/SideBar/SideBar.jsx
@@ -2,15 +2,15 @@ import './SideBar.scss';
 import ImageCrop from './ImageCrop/ImageCrop';
 import Frame from './Frame/Frame.jsx';
 
-const SideBar = props => {
+const SideBar = (props) => {
   const { cropImage, selectedIcon } = props;
   return (
     <>
       <div className='SideBar-wrapper'>
         {/* Either of the  Components ImageCrop or Frame should be shown at a time.
         Comment or uncomment the required Component accordingly.*/}
-        {/* <ImageCrop cropImage={cropImage} selectedIcon={selectedIcon} /> */}
-        <Frame />
+        <ImageCrop cropImage={cropImage} selectedIcon={selectedIcon} />
+        {/* <Frame /> */}
       </div>
     </>
   );
