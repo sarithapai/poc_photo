32503b2dfa535b183537e15ea1debd3958544114
akshitha23 <akshithasalian8@gmail.com>
2022-05-02 10:26:47 +0530

    Frame related Changes

--- src/Containers/Export/SideBar/Frame/Frame.jsx
+++ src/Containers/Export/SideBar/Frame/Frame.jsx
@@ -1,34 +1,38 @@
 import './Frame.scss';
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import FrameSetting from './FrameSetting.jsx';
+import { Icons } from '../../../../Utils/Icons';
+import { UNFRAMED, FRAMED } from '../../../../Utils/Constants';
+import { GlobalContext } from '../../../../Context/GlobalState';
 
 const Frame = () => {
   const frameTypes = [
     {
       id: 1,
-      image_path: '/download.jpeg',
-      name: 'Unframed',
+      image_path: Icons.unframed,
+      name: UNFRAMED,
       description:
         'We use acid-free mat boards. The neutral contrast of a white mat helps artwork really pop, creating a stunning viewing experience.'
     },
     {
       id: 2,
-      image_path: '/download.jpeg',
-      name: 'Framed',
+      image_path: Icons.framed,
+      name: FRAMED,
       description:
         'Our gallery frame are made with premium Italian pine. This frame’s slim face and pronounced depth is the preferred style by most artists and designers.'
     }
   ];
   const [selectedFrameId, setSelectedFrameId] = useState(null);
+  const { updateFrameType } = useContext(GlobalContext);
   return (
     <>
       <div className='frame-types-wrapper'>
         {frameTypes.map(frame => (
           <div
             key={frame.id}
-            // className='frame-wrapper'
             onClick={() => {
               setSelectedFrameId(frame.id);
+              updateFrameType(frame.name);
             }}
             className={
               frame.id === selectedFrameId
@@ -36,11 +40,9 @@ const Frame = () => {
                 : 'frame-wrapper'
             }
           >
+            <div></div>
             <div>
-              <img
-                alt=''
-                src='https://i.picsum.photos/id/212/700/700.jpg?hmac=GrEX7n4bVOZt4IjO2_WLLcoiQvPMQT7Sb8QfuXBjKoA'
-              />{' '}
+              <img alt='' src={frame.image_path} />{' '}
             </div>
             <div className='frame-detail'>
               <div> {frame.name}</div>
@@ -57,7 +59,3 @@ const Frame = () => {
 };
 
 export default Frame;
-
-// 1. if selected frame type is framed then show another
-// view with transition
-// also change img in framed type
--- src/Containers/Export/SideBar/Frame/FrameSetting.jsx
+++ src/Containers/Export/SideBar/Frame/FrameSetting.jsx
@@ -1,37 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import { FRAME_COLORS, FRAME_MATTINGS } from '../../../../Utils/Constants';
+import { GlobalContext } from '../../../../Context/GlobalState';
 
 const FrameSetting = () => {
   const colors = [
-    { id: 1, name: 'Nat', value: '#d5bea2' },
-    { id: 2, name: 'Black', value: '#352f2f' },
-    { id: 3, name: 'White', value: '#f6f0f0' }
+    { id: 1, name: 'Nat', value: FRAME_COLORS.NAT },
+    { id: 2, name: 'Black', value: FRAME_COLORS.BLACK },
+    { id: 3, name: 'White', value: FRAME_COLORS.WHITE }
   ];
   const mattingTypes = [
     {
       id: 1,
       name: 'Default',
+      value: FRAME_MATTINGS.DEFAULT,
       styleClass: 'color-item matting default-matting'
     },
     {
       id: 2,
       name: 'Medium',
+      value: FRAME_MATTINGS.MEDIUM,
       styleClass: 'color-item matting medium-matting'
     },
     {
       id: 3,
       name: 'Small',
+      value: FRAME_MATTINGS.SMALL,
       styleClass: 'color-item matting small-matting'
     }
   ];
   const [selectedColor, setSelectedColor] = useState(null);
   const [selectedmatting, setSelectedMatting] = useState(null);
+
+  const { updateFrameColor, updateFrameMatting } = useContext(GlobalContext);
+
   return (
-    <>
+    <div style={{ transition: '2s' }}>
       <div style={{ marginBottom: '30px' }}>
         <div className='frame-color-title'>Frame Color</div>
         <div className='color-box'>
           {colors.map(color => (
-            <div className='color-item-wrapper '>
+            <div className='color-item-wrapper ' key={color.id}>
               <div
                 key='name'
                 // className='color-item'
@@ -40,7 +48,10 @@ const FrameSetting = () => {
                     ? 'color-item active'
                     : 'color-item '
                 }
-                onClick={() => setSelectedColor(color.id)}
+                onClick={() => {
+                  setSelectedColor(color.id);
+                  updateFrameColor(color.value);
+                }}
                 style={{ backgroundColor: color.value }}
               ></div>
               <div>{color.name}</div>
@@ -53,14 +64,17 @@ const FrameSetting = () => {
         <div className='frame-color-title'>Matting</div>
         <div className='color-box'>
           {mattingTypes.map(matting => (
-            <div className='color-item-wrapper'>
+            <div className='color-item-wrapper' key={matting.id}>
               <div
                 className={
                   selectedmatting === matting.id
                     ? matting.styleClass + ' active'
                     : matting.styleClass
                 }
-                onClick={() => setSelectedMatting(matting.id)}
+                onClick={() => {
+                  setSelectedMatting(matting.id);
+                  updateFrameMatting(matting.value);
+                }}
                 // className={}
               ></div>
 
@@ -69,7 +83,7 @@ const FrameSetting = () => {
           ))}
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
