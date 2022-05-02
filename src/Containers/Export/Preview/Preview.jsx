f3807fbae5c0810e6b3e36af30593f92b2e43a4f
akshitha23 <akshithasalian8@gmail.com>
2022-05-02 10:34:37 +0530

    Preview Changes

--- src/Containers/Export/Preview/Preview.jsx
+++ src/Containers/Export/Preview/Preview.jsx
@@ -3,24 +3,61 @@ import { GlobalContext } from '../../../Context/GlobalState';
 import ReactCrop from 'react-image-crop';
 import 'react-image-crop/src/ReactCrop.scss';
 import './Preview.scss';
+import {
+  FRAMED,
+  PAGE_SIZE_SELECTION,
+  PAGE_FRAME_SELECTION
+} from '../../../Utils/Constants';
 
 const Preview = () => {
-  const { preview } = useContext(GlobalContext);
+  const {
+    preview,
+    activePageIndex,
+    frameColor,
+    frameMatting,
+    selectedFrameType
+  } = useContext(GlobalContext);
   const [crop, setCrop] = useState({
     unit: '%',
     width: 300,
-    aspect: 16 / 9,
+    aspect: 16 / 9
   });
 
-  const onCropChange = (inCrop) => {
+  const onCropChange = inCrop => {
     setCrop(inCrop);
   };
 
   return (
     <div className='preview-wrapper'>
-      <ReactCrop crop={crop} onChange={onCropChange}>
-        <img src={preview}></img>
-      </ReactCrop>
+      {activePageIndex === PAGE_SIZE_SELECTION ? (
+        <ReactCrop crop={crop} onChange={onCropChange}>
+          <img src={preview} alt=''></img>
+        </ReactCrop>
+      ) : activePageIndex === PAGE_FRAME_SELECTION ? (
+        selectedFrameType === FRAMED ? (
+          <div
+            className='frame'
+            style={{
+              border: '18px solid ' + frameColor,
+              padding: frameMatting + 'px'
+            }}
+          >
+            <img
+              id='preview-image'
+              alt=''
+              className={'preview-image'}
+              src={preview}
+            />
+          </div>
+        ) : (
+          <img
+            id='preview-image'
+            alt=''
+            className={'preview-image'}
+            src={preview}
+          />
+        )
+      ) : null}
     </div>
   );
 };
